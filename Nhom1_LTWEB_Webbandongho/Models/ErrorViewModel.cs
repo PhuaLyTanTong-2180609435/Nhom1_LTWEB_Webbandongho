a98013c5063eea8ff22e9d5f29be78b04f18bb72
PhuaLyTanTong-2180609435 <phuakytantong@gmail.com>
2024-04-04 19:16:54 +0700

    Add project files.

---
+++ Nhom1_LTWEB_Webbandongho/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nhom1_LTWEB_Webbandongho.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
